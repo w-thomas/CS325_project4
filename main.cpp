911770518a5c98aafe518bb9292a3ba0d0b02d38
mstryff <stryffem@onid.oregonstate.edu>
2016-03-10 15:03:06 -0800

    Fixed file output

--- main.cpp
+++ main.cpp
@@ -28,46 +28,51 @@ int main(int argc, char* argv[])
   	int numCities = adjacencyMatrix.size();
 
 
-  clock_t startTime, endTime;
-  startTime = clock();
+  	clock_t startTime, endTime;
+  	startTime = clock();
 	//Get nearest neighbor tour
 	std::vector<int> tour = nnTour(adjacencyMatrix, numCities);
 
 	
-	std::ofstream file;
-	std::string fName = argv[1];
-	fName += ".tour";
-	file.open(fName);
 	
 	//Get and output cost
 	int cost = getCost(adjacencyMatrix, tour);
 	std::cout << "Total cost: " << cost << std::endl;
-	
-	file << cost << '\n';
-	
+		
 	//Output tour
 	std::cout << "Nearest Neighbor Tour" << std::endl;
 	for(int i = 0; i <= numCities-1; i++)
 	{
 		std::cout << tour[i] << '\t';
-		file << tour[i] << '\n';
 	}
 	std::cout << std::endl;
 
-  file.close();
-	endTime = clock();
-	endTime -= startTime;
-
 	//Get 2opt tour
 	std::vector<int> two_optTour = two_opt(tour, adjacencyMatrix, numCities, endTime);
+
+	//Open output file
+	std::ofstream file;
+	std::string fName = argv[1];
+	fName += ".tour";
+	file.open(fName);
+
+	//Get and output cost
+	cost = getCost(adjacencyMatrix, two_optTour);
+	file << cost << '\n';
+	std::cout << "Total cost: " << cost << std::endl;
 
 	//Output tour
 	std::cout << "After Pairwise Comparison" << std::endl;
 	for(int i = 0; i <= numCities; i++)
 	{
 		std::cout << two_optTour[i] << '\t';
+		file << two_optTour[i] << '\n';
 	}
 	std::cout << std::endl;
+
+  	file.close();
+	endTime = clock();
+	endTime -= startTime;
 
 	//Get and output cost
 	cost = getCost(adjacencyMatrix, two_optTour);
