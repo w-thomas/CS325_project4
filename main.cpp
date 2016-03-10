049aa52520396b4f47188b583104e5365714de07
calebbernard <calebbernard@gmail.com>
2016-03-10 16:27:22 -0600

    Now includes file output

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ Description: 	Nearest neighbor example program.
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
@@ -23,23 +24,34 @@ int main(int argc, char* argv[])
 
 	//Get input
 	std::vector< std::vector<int> > adjacencyMatrix;
-  	get_input(argv[1], adjacencyMatrix);	
+  	get_input(argv[1], adjacencyMatrix);
   	int numCities = adjacencyMatrix.size();
 
 	//Get nearest neighbor tour
 	std::vector<int> tour = nnTour(adjacencyMatrix, numCities);
 	
-	//Output tour
-	std::cout << "Nearest Neighbor Tour" << std::endl;
-	for(int i = 0; i <= numCities; i++)
-	{
-		std::cout << tour[i] << '\t';
-	}
-	std::cout << std::endl;
-
+	std::ofstream file;
+	std::string fName = argv[1];
+	fName += ".tour";
+	file.open(fName);
+	
 	//Get and output cost
 	int cost = getCost(adjacencyMatrix, tour);
 	std::cout << "Total cost: " << cost << std::endl;
+	
+	file << cost << '\n';
+	
+	//Output tour
+	std::cout << "Nearest Neighbor Tour" << std::endl;
+	for(int i = 0; i <= numCities-1; i++)
+	{
+		std::cout << tour[i] << '\t';
+		file << tour[i] << '\n';
+	}
+	std::cout << std::endl;
+
+  file.close();
+	
 
 	//Get 2opt tour
 	std::vector<int> two_optTour = two_opt(tour, adjacencyMatrix, numCities);
