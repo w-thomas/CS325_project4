355793329283f95e45e089021ad38b84337a60c1
calebbernard <calebbernard@gmail.com>
2016-03-10 00:31:05 -0600

    Maybe a slight improvement

--- tsp.cpp
+++ tsp.cpp
@@ -14,6 +14,7 @@ class point{
 void get_input(char* fName, std::vector< std::vector<int> >& matrix){
   std::vector<point> pointList;
   int i, number, x, y, switcher = 1;
+  std::cout << "here1\n";
   std::ifstream file;
   file.open(fName);
   while (file >> i){
@@ -40,19 +41,21 @@ void get_input(char* fName, std::vector< std::vector<int> >& matrix){
     pointList.push_back(a);
   }
   file.close();
-  
+  std::cout << "here2\n";
   // Now we want to turn the list of points into an adjacency matrix.
   int numPoints = pointList.size();
   for (int a = 0; a < numPoints; a++){
     for (int b = 0; b < numPoints; b++){
+      std::cout << pointList[a].x;
       matrix[a][b] = round(sqrt((pointList[a].x - pointList[b].x)^2 + (pointList[a].y - pointList[b].y)^2));
     }
   }
+  std::cout << "Here3\n";
   
 }
 
 int main(int argc, char** argv){
   std::vector< std::vector<int> > matrix;
-  get_input(argv[1], &matrix);
+  get_input(argv[1], matrix);
   return 0;
 }
